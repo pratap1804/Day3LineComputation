91f6789825171611665c9cea4e56d59b50e9763b
pratap1804 <pratap.1804@gmail.com>
2021-06-24 05:54:55 +0530

    Given two lines checking if lines are equal greater or lesser than another

---
+++ UserCase3.java
@@ -0,0 +1,30 @@
+public class LineComputation {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line Computation problem");
+
+        int x1=23;
+        int y1=74;
+        int x2=45;
+        int y2=65;
+
+        double length1 = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+        System.out.println("Length of the line is: "+length1+" unit");
+
+        int x3=13;
+        int y3=74;
+        int x4=35;
+        int y4=65;
+
+        double length2 = Math.sqrt((x4-x3)*(x4-x3)+(y4-y3)*(y4-y3));
+        System.out.println("Length of the line is: "+length2+" unit");
+
+        if (length1 == length2) {
+            System.out.println("Lines Are Equal");
+        } else if
+            (length1 > length2) {
+                System.out.println("Line 1 is greater than line 2");
+        }
+        else
+            System.out.println("line 2 is greater than line 1");
+        }
+    }
